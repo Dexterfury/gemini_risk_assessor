b3fc6e9289f9c5fbff5cb6b84907637af1404ddd
Dexterfury <raphaelsquared@gmail.com>
2024-06-11 00:19:53 +0200

    updates

--- lib/screens/risk_assessment_details_screen.dart
+++ lib/screens/risk_assessment_details_screen.dart
@@ -23,101 +23,208 @@ class RiskAssessmentDetailsScreen extends StatelessWidget {
       child: ScaleTransition(
         scale: Tween(begin: 3.0, end: 1.0).animate(animation),
         child: Scaffold(
+          appBar: AppBar(
+            title: Text(assessmentModel.title),
+          ),
           body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // title
-                  HeadingTitleWidget(
-                    title: assessmentModel.title,
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Task to Achieve:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(assessmentModel.taskToAchieve),
+                SizedBox(height: 10),
+                Text(
+                  'Equipments:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.equipments
+                    .map((equipment) => Text(equipment)),
+                SizedBox(height: 10),
+                Text(
+                  'Hazards:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.hazards.map((hazard) => Text(hazard)),
+                SizedBox(height: 10),
+                Text(
+                  'Risks:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.risks.map((risk) => Text(risk)),
+                SizedBox(height: 10),
+                Text(
+                  'Control Measures:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.control
+                    .map((controlMeasure) => Text(controlMeasure)),
+                SizedBox(height: 10),
+                Text(
+                  'Personal Protective Equipment (PPE):',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  // tast to archieve
-                  Row(
-                    children: [
-                      const Text(
-                        'Tast: ',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(assessmentModel.taskToAchieve),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  itemCount: assessmentModel.ppe.length,
+                  itemBuilder: (context, index) {
+                    return Column(
                       children: [
-                        DataItemsWidget(
-                          label: 'Equipments',
-                          dataList: assessmentModel.equipments,
-                        ),
-                        DataItemsWidget(
-                          label: 'Hazards',
-                          dataList: assessmentModel.hazards,
-                        ),
+                        Icon(Icons.security), // Replace with appropriate icons
+                        Text(assessmentModel.ppe[index]),
                       ],
+                    );
+                  },
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Summary:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(assessmentModel.summary),
+                SizedBox(height: 10),
+                Text(
+                  'Created by: ${assessmentModel.createdBy}',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text('Created at: ${assessmentModel.createdAt.toLocal()}'),
+                SizedBox(height: 10),
+                Text(
+                  'Signatures:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.signatures
+                    .map((signature) => Text(signature)),
+                SizedBox(height: 10),
+                Text(
+                  'Approvers:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                ...assessmentModel.approvers.map((approver) => Text(approver)),
+                SizedBox(height: 10),
+                Text(
+                  'Add Signature:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 150,
+                  child: Container(
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Text('Signature Box'),
                     ),
                   ),
-
-                  const SizedBox(
-                    height: 10,
-                  ),
-
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        DataItemsWidget(
-                          label: 'Risks',
-                          dataList: assessmentModel.risks,
-                        ),
-                        PpeItemsWidget(
-                          label: 'Personal Protective Equipment',
-                          ppeModelList:
-                              context.watch<AssessmentProvider>().ppeModelList,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-
-                  Column(
-                    children: [
-                      const Text(
-                        'Control Messueres',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      DataListWidget(dataList: assessmentModel.control),
-                    ],
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(false);
-                    },
-                    child: Text('Close'),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
+
+        // Scaffold(
+        //   body: SingleChildScrollView(
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: [
+        //           // title
+        //           HeadingTitleWidget(
+        //             title: assessmentModel.title,
+        //           ),
+        //           const SizedBox(
+        //             height: 10,
+        //           ),
+        //           // tast to archieve
+        //           Row(
+        //             children: [
+        //               const Text(
+        //                 'Tast: ',
+        //                 style: TextStyle(fontWeight: FontWeight.bold),
+        //               ),
+        //               Text(assessmentModel.taskToAchieve),
+        //             ],
+        //           ),
+        //           const SizedBox(
+        //             height: 10,
+        //           ),
+
+        //           SizedBox(
+        //             width: MediaQuery.of(context).size.width,
+        //             child: Row(
+        //               crossAxisAlignment: CrossAxisAlignment.start,
+        //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //               children: [
+        //                 DataItemsWidget(
+        //                   label: 'Equipments',
+        //                   dataList: assessmentModel.equipments,
+        //                 ),
+        //                 DataItemsWidget(
+        //                   label: 'Hazards',
+        //                   dataList: assessmentModel.hazards,
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+
+        //           const SizedBox(
+        //             height: 10,
+        //           ),
+
+        //           SizedBox(
+        //             width: MediaQuery.of(context).size.width,
+        //             child: Row(
+        //               crossAxisAlignment: CrossAxisAlignment.start,
+        //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //               children: [
+        //                 DataItemsWidget(
+        //                   label: 'Risks',
+        //                   dataList: assessmentModel.risks,
+        //                 ),
+        //                 PpeItemsWidget(
+        //                   label: 'Personal Protective Equipment',
+        //                   ppeModelList:
+        //                       context.watch<AssessmentProvider>().ppeModelList,
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //           const SizedBox(
+        //             height: 10,
+        //           ),
+
+        //           Column(
+        //             children: [
+        //               const Text(
+        //                 'Control Messueres',
+        //                 style: TextStyle(fontWeight: FontWeight.bold),
+        //               ),
+        //               DataListWidget(dataList: assessmentModel.control),
+        //             ],
+        //           ),
+
+        //           const SizedBox(
+        //             height: 20,
+        //           ),
+        //           ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.of(context).pop(false);
+        //             },
+        //             child: Text('Close'),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
