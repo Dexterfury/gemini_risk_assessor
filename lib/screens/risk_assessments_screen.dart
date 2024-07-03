d4864b46e936ae65d3aac2fcdf4deca5ac4c88ba
Dexterfury <raphaelsquared@gmail.com>
2024-07-03 16:56:41 +0200

    updates

--- lib/screens/risk_assessments_screen.dart
+++ lib/screens/risk_assessments_screen.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gemini_risk_assessor/appBars/my_app_bar.dart';
+import 'package:gemini_risk_assessor/appBars/my_sliver_app_bar.dart';
 import 'package:gemini_risk_assessor/constants.dart';
 import 'package:gemini_risk_assessor/models/assessment_model.dart';
 import 'package:gemini_risk_assessor/providers/assessment_provider.dart';
 import 'package:gemini_risk_assessor/providers/auth_provider.dart';
 import 'package:gemini_risk_assessor/themes/my_themes.dart';
 import 'package:gemini_risk_assessor/widgets/list_item.dart';
-import 'package:gemini_risk_assessor/appBars/my_app_bar.dart';
 import 'package:provider/provider.dart';
 
 class RistAssessmentsScreen extends StatelessWidget {
@@ -21,14 +22,12 @@ class RistAssessmentsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final uid = context.read<AuthProvider>().userModel!.uid;
     final assessmentProvider = context.read<AssessmentProvider>();
+
+    handleSearch(String query) {
+      // Implement your search logic here
+      //context.read<TabProvider>().setSearchQuery(query);
+    }
     return Scaffold(
-      appBar: orgID.isNotEmpty
-          ? MyAppBar(
-              leading: const BackButton(),
-              title: '',
-              onSearch: _handleSearch,
-            )
-          : null,
       body: SafeArea(
         child: StreamBuilder<QuerySnapshot>(
           stream: assessmentProvider.ristAssessmentsStream(
@@ -48,36 +47,45 @@ class RistAssessmentsScreen extends StatelessWidget {
             }
 
             if (snapshot.data!.docs.isEmpty) {
-              return const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('No Risk Assessments found',
-                      textAlign: TextAlign.center, style: textStyle18w500),
+              return Scaffold(
+                appBar: orgID.isNotEmpty
+                    ? const MyAppBar(
+                        leading: BackButton(),
+                        title: Constants.riskAssessments,
+                      )
+                    : null,
+                body: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('No Risk Assessments Yet!',
+                        textAlign: TextAlign.center, style: textStyle18w500),
+                  ),
                 ),
               );
             }
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final doc = snapshot.data!.docs[index];
-                  final data = doc.data() as Map<String, dynamic>;
-                  final assessment = AssessmentModel.fromJson(data);
-                  return ListItem(
-                    data: assessment,
+            return orgID.isNotEmpty
+                ? MySliverAppBar(
+                    snapshot: snapshot,
+                    title: Constants.riskAssessments,
+                    onSearch: handleSearch,
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final doc = snapshot.data!.docs[index];
+                        final data = doc.data() as Map<String, dynamic>;
+                        final assessment = AssessmentModel.fromJson(data);
+                        return ListItem(
+                          data: assessment,
+                        );
+                      },
+                    ),
                   );
-                },
-              ),
-            );
           },
         ),
       ),
     );
-  }
-
-  void _handleSearch(String query) {
-    print('Searching for "$query" in tab: ');
-    // Implement your search logic here
   }
 }
--- lib/screens/tools_screen.dart
+++ lib/screens/tools_screen.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gemini_risk_assessor/appBars/my_app_bar.dart';
+import 'package:gemini_risk_assessor/appBars/my_sliver_app_bar.dart';
 import 'package:gemini_risk_assessor/constants.dart';
+import 'package:gemini_risk_assessor/models/tool_model.dart';
 import 'package:gemini_risk_assessor/providers/auth_provider.dart';
 import 'package:gemini_risk_assessor/providers/tool_provider.dart';
 import 'package:gemini_risk_assessor/streams/tools_stream.dart';
-import 'package:gemini_risk_assessor/appBars/my_app_bar.dart';
+import 'package:gemini_risk_assessor/themes/my_themes.dart';
+import 'package:gemini_risk_assessor/widgets/grid_item.dart';
 import 'package:provider/provider.dart';
 
 class ToolsScreen extends StatelessWidget {
@@ -18,26 +23,72 @@ class ToolsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final uid = context.read<AuthProvider>().userModel!.uid;
     final toolProvider = context.read<ToolsProvider>();
+
+    handleSearch(String query) {
+      // Implement your search logic here
+      //context.read<TabProvider>().setSearchQuery(query);
+    }
     return Scaffold(
-      appBar: orgID.isNotEmpty
-          ? MyAppBar(
-              leading: const BackButton(),
-              title: '',
-              onSearch: _handleSearch,
-            )
-          : null,
       body: SafeArea(
-        child: ToolsStream(
-          toolProvider: toolProvider,
-          uid: uid,
-          orgID: orgID,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: toolProvider.toolsStream(
+            userId: uid,
+            orgID: orgID,
+          ),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            if (snapshot.hasError) {
+              return const Center(child: Text('Something went wrong'));
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.data!.docs.isEmpty) {
+              return Scaffold(
+                appBar: orgID.isNotEmpty
+                    ? const MyAppBar(
+                        leading: BackButton(),
+                        title: Constants.tools,
+                      )
+                    : null,
+                body: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'You have not saved any tools',
+                      textAlign: TextAlign.center,
+                      style: textStyle18w500,
+                    ),
+                  ),
+                ),
+              );
+            }
+            return orgID.isNotEmpty
+                ? MySliverAppBar(
+                    snapshot: snapshot,
+                    title: Constants.tools,
+                    onSearch: handleSearch,
+                  )
+                : GridView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1,
+                    ),
+                    itemBuilder: (context, index) {
+                      final tool = ToolModel.fromJson(
+                          snapshot.data!.docs[index] as Map<String, dynamic>);
+                      return GridItem(toolModel: tool);
+                    },
+                  );
+          },
         ),
       ),
     );
-  }
-
-  void _handleSearch(String query) {
-    print('Searching for "$query" in tab: ');
-    // Implement your search logic here
   }
 }
