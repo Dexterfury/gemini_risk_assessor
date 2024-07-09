6969457db1ca30c53b78df3e0a6aae743c2ca6cd
Dexterfury <raphaelsquared@gmail.com>
2024-07-09 23:10:46 +0200

    updates

--- lib/screens/organisations_screen.dart
+++ lib/screens/organisations_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_risk_assessor/constants.dart';
 import 'package:gemini_risk_assessor/providers/auth_provider.dart';
 import 'package:gemini_risk_assessor/providers/organisation_provider.dart';
 import 'package:gemini_risk_assessor/search/org_serach_stream.dart';
 import 'package:gemini_risk_assessor/streams/organisations_stream.dart';
+import 'package:gemini_risk_assessor/utilities/navigation.dart';
 import 'package:gemini_risk_assessor/widgets/anonymouse_view.dart';
 import 'package:gemini_risk_assessor/widgets/display_user_image.dart';
 import 'package:gemini_risk_assessor/appBars/my_app_bar.dart';
@@ -28,12 +30,7 @@ class OrganisationsScreen extends StatelessWidget {
           title: '',
           onSearch: handleSearch, // Pass the search function
           actions: [
-            DisplayUserImage(
-              radius: 20,
-              isViewOnly: true,
-              imageUrl: context.watch<AuthProvider>().userModel?.imageUrl ?? '',
-              onPressed: () {},
-            ),
+            _buildUserImage(context),
           ],
         ),
         body: isAnonymous
@@ -43,5 +40,25 @@ class OrganisationsScreen extends StatelessWidget {
                 : const OrgSearchStream(),
       );
     });
+  }
+
+  GestureDetector _buildUserImage(
+    BuildContext context,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        navigationController(
+          context: context,
+          route: Constants.profileRoute,
+          titleArg: context.read<AuthProvider>().userModel!.uid,
+        );
+      },
+      child: DisplayUserImage(
+        radius: 20,
+        isViewOnly: true,
+        imageUrl: context.watch<AuthProvider>().userModel?.imageUrl ?? '',
+        onPressed: () {},
+      ),
+    );
   }
 }
