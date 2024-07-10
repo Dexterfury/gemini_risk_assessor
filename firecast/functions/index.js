15e09d011fd082ce667e0ba9eb7de6704aadb056
Dexterfury <raphaelsquared@gmail.com>
2024-07-10 23:41:13 +0200

    added cloud functions

--- firecast/functions/index.js
+++ firecast/functions/index.js
@@ -7,9 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const { log } = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
