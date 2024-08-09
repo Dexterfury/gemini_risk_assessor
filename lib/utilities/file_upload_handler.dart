7210c52a955e500bda563d6cac1cb900ce67204e
Dexterfury <raphaelsquared@gmail.com>
2024-08-09 08:16:46 +0200

    updated file upload handler with error handling

--- lib/utilities/file_upload_handler.dart
+++ lib/utilities/file_upload_handler.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:gemini_risk_assessor/constants.dart';
+import 'package:gemini_risk_assessor/enums/enums.dart';
+import 'package:gemini_risk_assessor/firebase/error_handler.dart';
 import 'package:path/path.dart' as path;
 
 class FileUploadHandler {
@@ -11,81 +13,123 @@ class FileUploadHandler {
     required File file,
     required String reference,
   }) async {
-    print("Original file path: ${file.path}");
+    try {
+      final String extension = path.extension(file.path).toLowerCase();
+      File fileToUpload;
 
-    final String extension = path.extension(file.path).toLowerCase();
-    File fileToUpload;
+      if (extension == '.pdf') {
+        fileToUpload = file;
+      } else if (['.jpg', '.jpeg', '.png'].contains(extension)) {
+        fileToUpload = await compressAndGetFile(
+          file: file,
+          targetPath: file.path.replaceAll(extension, '_compressed$extension'),
+        );
+      } else {
+        throw UnsupportedError('Unsupported file type: $extension');
+      }
 
-    if (extension == '.pdf') {
-      fileToUpload = file;
-    } else if (['.jpg', '.jpeg', '.png'].contains(extension)) {
-      fileToUpload = await compressAndGetFile(
-        file: file,
-        targetPath: file.path.replaceAll(extension, '_compressed$extension'),
+      String downloadUrl = await storeFileToStorage(
+        file: fileToUpload,
+        reference: reference,
       );
-    } else {
-      throw UnsupportedError('Unsupported file type: $extension');
+      return downloadUrl;
+    } catch (e, stackTrace) {
+      ErrorHandler.recordError(
+        e,
+        stackTrace,
+        reason: 'Failed to upload file and get URL',
+        severity: ErrorSeverity.medium,
+      );
+      rethrow; // Optionally rethrow the error if you want the calling method to handle it
     }
-
-    String downloadUrl = await storeFileToStorage(
-      file: fileToUpload,
-      reference: reference,
-    );
-    return downloadUrl;
   }
 
   static Future<String> storeFileToStorage({
     required File file,
     required String reference,
   }) async {
-    if (!file.existsSync()) {
-      throw FileSystemException("File does not exist", file.path);
-    }
+    try {
+      if (!file.existsSync()) {
+        throw FileSystemException("File does not exist", file.path);
+      }
 
-    UploadTask uploadTask =
-        FirebaseStorage.instance.ref().child(reference).putFile(file);
-    TaskSnapshot taskSnapshot = await uploadTask;
-    String fileUrl = await taskSnapshot.ref.getDownloadURL();
-    return fileUrl;
+      UploadTask uploadTask =
+          FirebaseStorage.instance.ref().child(reference).putFile(file);
+      TaskSnapshot taskSnapshot = await uploadTask;
+      String fileUrl = await taskSnapshot.ref.getDownloadURL();
+      return fileUrl;
+    } catch (e, stackTrace) {
+      ErrorHandler.recordError(
+        e,
+        stackTrace,
+        reason: 'Failed to store file to storage',
+        severity: ErrorSeverity.high,
+      );
+      rethrow;
+    }
   }
 
   static Future<File> compressAndGetFile({
     required File file,
     required String targetPath,
   }) async {
-    var result = await FlutterImageCompress.compressAndGetFile(
-      file.absolute.path,
-      targetPath,
-      quality: 90,
-    );
+    try {
+      var result = await FlutterImageCompress.compressAndGetFile(
+        file.absolute.path,
+        targetPath,
+        quality: 90,
+      );
 
-    return File(result!.path);
+      if (result == null) {
+        throw Exception("Failed to compress image");
+      }
+
+      return File(result.path);
+    } catch (e, stackTrace) {
+      ErrorHandler.recordError(
+        e,
+        stackTrace,
+        reason: 'Failed to compress image file',
+        severity: ErrorSeverity.medium,
+      );
+      rethrow;
+    }
   }
 
-  // update image in firestore
+  // Update image in Firestore with error handling
   static Future<String> updateImage({
     required File file,
     required bool isUser,
     required String id,
     required String reference,
   }) async {
-    final imageURL = await uploadFileAndGetUrl(
-      file: file,
-      reference: reference,
-    );
-    if (isUser) {
-      await FirebaseAuth.instance.currentUser!.updatePhotoURL(imageURL);
-      await FirebaseFirestore.instance
-          .collection(Constants.usersCollection)
-          .doc(id)
-          .update({Constants.imageUrl: imageURL});
-    } else {
-      await FirebaseFirestore.instance
-          .collection(Constants.groupsCollection)
-          .doc(id)
-          .update({Constants.imageUrl: imageURL});
-    }
+    try {
+      final imageURL = await uploadFileAndGetUrl(
+        file: file,
+        reference: reference,
+      );
+      if (isUser) {
+        await FirebaseAuth.instance.currentUser!.updatePhotoURL(imageURL);
+        await FirebaseFirestore.instance
+            .collection(Constants.usersCollection)
+            .doc(id)
+            .update({Constants.imageUrl: imageURL});
+      } else {
+        await FirebaseFirestore.instance
+            .collection(Constants.groupsCollection)
+            .doc(id)
+            .update({Constants.imageUrl: imageURL});
+      }
 
-    return imageURL;
+      return imageURL;
+    } catch (e, stackTrace) {
+      ErrorHandler.recordError(
+        e,
+        stackTrace,
+        reason: 'Failed to update image in Firestore',
+        severity: ErrorSeverity.high,
+      );
+      rethrow;
+    }
   }
 }
